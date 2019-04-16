0bd36961e0123ff49c782f042bfaa7c25a980f64
ramo <knht31i@gmail.com>
2019-04-17 01:48:12 +0900

    rdの変更等+不要ファイルの削除

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 CSVをfirebaseのCloud Firestoreにアップロードするためのプログラム
 
+```
+pip install -r requieremnts.txt
+python main.py
+```
+にて、`main.csv`の中身がデータベースにアップロードされる。  
+独自の形式のため、他のcsvを使用する際は`main.py`の中身を書き換えて実行する。 
+
+```module.py```  
+csvの値を使いやすいように整形する用のプログラム
+
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import firebase_admin
-from module import Majoreco,Majoredu,Majortou,Majoreng,Targett,Termm,Terme,Dayday,Periodd
+from module import Majoreco,Majoredu,Majortou,Majoreng,Targett,Termm,Terme,Dayday,Periodd,Daydayday,Majorpan
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-cred = credentials.Certificate('key2.json')
+cred = credentials.Certificate('key2.json')#APIKEY
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -12,9 +12,8 @@ db = firestore.client()
 
 
 import csv
-with open('some.csv', 'r') as f:
+with open('main.csv', 'r') as f:
     reader = csv.reader(f)
-    header = next(reader)
     no = 1
 
     for row in reader:
@@ -22,27 +21,44 @@ with open('some.csv', 'r') as f:
         eco = Majoreco(row[0])
         edu = Majoredu(row[0])
         eng = Majoreng(row[0])
+        pan = Majorpan(row[0])
         title = row[1]
         teacher = row[2]
-        target = Targett(row[3])
-        term = Termm(row[4])
-        termex = Terme(row[4])
-        day = Dayday(row[5])
-        period = Periodd(row[5])
-        doc_ref = db.collection(u'syllabus').document(title)
+        target = row[3]
+        day = Daydayday(row[4])
+        term = row[5]
+        kubun = row[6]
+        kazu = row[7]
+        overview = row[8]
+        goal = row[9]
+        evaluation = row[10]
+        text = row[11]
+        text2 = row[12]
+        message = row[13]
+        learning = row[14]
+
+        doc_ref = db.collection(u'syllabus-comp').document(title)
         data = {
-            u'no': no, #通し番号
             u'tourism': tou,#trueの場合その学部の授業
             u'economics': eco,#trueの場合その学部の授業
             u'education': edu,#trueの場合その学部の授業
             u'engineering': eng,#trueの場合その学部の授業
-            u'title': title,#授業名
-            u'teacher': teacher,#教師の名前
-            u'target': target,#何回生向けか
+            u'culture': pan,
+            u'no': no, #通し番号
+            u'title': title, #授業名
+            u'teacher': teacher, #教師の名前
+            u'target': target,  #何回生向けか
+            u'day': day,#空欄は8
             u'term': term, #Trueが前期Falseが後期
-            u'term-ex': termex, #term-exception 通年などの例外
-            u'day': day, #日曜日が0 時間外は7 空欄は8
-            u'period': period, #何限目か 時間外は7 空欄は8
+            u'kubun': kubun, #
+            u'kazu': kazu, #単位数
+            u'overview': overview,#概要
+            u'goal': goal,#目標
+            u'evaluation': evaluation,#評価方法
+            u'text': text,#教科書
+            u'text2': text2,#参考文献
+            u'message': message,#メッセージ
+            u'learning': learning,#学習方法
         }
         doc_ref.set(data) #送信
         print(no)
